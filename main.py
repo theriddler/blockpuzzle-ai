5ea16b7444bd8fd888318dbfafc05dfc9f505482
theriddler <riddle.hunter@gmail.com>
2023-10-05 05:49:12 -0400

    can place blocks

--- main.py
+++ main.py
@@ -8,11 +8,11 @@ BLACK = (0,0,0)
 
 # consts
 BLOCK_SIZE = 60
-BLOCK_IN_X = 10
-BLOCK_IN_Y = 10
+BLOCKS_IN_WIDTH = 10
+BLOCKS_IN_HEIGHT = 10
 
-width = BLOCK_SIZE * BLOCK_IN_X
-height = BLOCK_SIZE * BLOCK_IN_Y
+PIXEL_WIDTH = BLOCK_SIZE * BLOCKS_IN_WIDTH
+PIXEL_HEIGHT = BLOCK_SIZE * BLOCKS_IN_HEIGHT
 
 # block types
 # read left to right, top to bottom
@@ -68,32 +68,59 @@ blocks = [
     BIG_L_BOTTOM_RIGHT
 ]
 
+# instantiate a board for us to keep track
+board = [[0 for x in range(BLOCKS_IN_WIDTH)] for y in range(BLOCKS_IN_HEIGHT)]
+
 def choose_block():
   return random.choice(blocks)
 
-def draw_grid():
-    for x in range(0, height, BLOCK_SIZE):
-        pygame.draw.line(screen, WHITE, (x, 0), (x, height))
-    for y in range(0, width, BLOCK_SIZE):
-        pygame.draw.line(screen, WHITE, (0, y), (width, y))
+def draw_board():
+  # grid
+  for x in range(0, PIXEL_HEIGHT, BLOCK_SIZE):
+      pygame.draw.line(screen, WHITE, (x, 0), (x, PIXEL_HEIGHT))
+  for y in range(0, PIXEL_WIDTH, BLOCK_SIZE):
+      pygame.draw.line(screen, WHITE, (0, y), (PIXEL_WIDTH, y))
+    
+  # already placed pieces
+  for xIdx in range(0, BLOCKS_IN_WIDTH):
+    for yIdx in range(0, BLOCKS_IN_WIDTH):
+       if(board[yIdx][xIdx] == 1):
+          pygame.draw.rect(screen, WHITE, ((xIdx*BLOCK_SIZE), (yIdx*BLOCK_SIZE), BLOCK_SIZE, BLOCK_SIZE))
+
+def mousepos_to_block_top_left(mousePos):
+  (mouseX,mouseY) = mousePos
+
+  # get to the root of the block (top left)
+  x = mouseX - (mouseX%BLOCK_SIZE)
+  y = mouseY - (mouseY%BLOCK_SIZE)
+
+  return (x, y)
+
 
 def draw_current_block(mousePos, currentBlock):
-    (mouseX,mouseY) = mousePos
+  (x, y) = mousepos_to_block_top_left(mousePos)
 
-    # get to the root of the block (top left)
-    xStart = mouseX - (mouseX%BLOCK_SIZE)
-    yStart = mouseY - (mouseY%BLOCK_SIZE)
+  for yIdx in range(0,3):
+      for xIdx in range(0,3):
+          if(currentBlock[yIdx][xIdx] == 1):
+            pygame.draw.rect(screen, WHITE, ((x+((xIdx-1)*BLOCK_SIZE)), (y+((yIdx-1)*BLOCK_SIZE)), BLOCK_SIZE, BLOCK_SIZE))
 
-    for yIdx in range(0,3):
-        for xIdx in range(0,3):
-            occupied = currentBlock[yIdx][xIdx]
-            print(occupied)
-            if(occupied == 1):
-              pygame.draw.rect(screen, WHITE, ((xStart+((xIdx-1)*BLOCK_SIZE)), (yStart+((yIdx-1)*BLOCK_SIZE)), BLOCK_SIZE, BLOCK_SIZE))
+
+def set_current_block(mousePos, currentBlock):
+  (x, y) = mousepos_to_block_top_left(mousePos)
+
+  for yIdx in range(0,3):
+      for xIdx in range(0,3):
+          if(currentBlock[yIdx][xIdx] == 1):
+            board[int(y/BLOCK_SIZE)+(yIdx-1)][int(x/BLOCK_SIZE)+(xIdx-1)] = 1
+   
+  print(board)
+
+
 
 # pygame setup
 pygame.init()
-screen = pygame.display.set_mode((BLOCK_SIZE*BLOCK_IN_X, BLOCK_SIZE*BLOCK_IN_Y))
+screen = pygame.display.set_mode((BLOCK_SIZE*BLOCKS_IN_WIDTH, BLOCK_SIZE*BLOCKS_IN_HEIGHT))
 clock = pygame.time.Clock()
 running = True
 
@@ -106,13 +133,12 @@ while running:
       if event.type == pygame.QUIT:
           running = False
         
-      if event.type == pygame.KEYDOWN:
-        if event.key == pygame.K_SPACE:
-          currentBlock = choose_block()
+      if event.type == pygame.MOUSEBUTTONUP:
+        set_current_block(pygame.mouse.get_pos(), currentBlock)
 
   # set our background
   screen.fill(BLACK)
-  draw_grid()
+  draw_board()
 
   # draw current rect
   draw_current_block(pygame.mouse.get_pos(), currentBlock)
